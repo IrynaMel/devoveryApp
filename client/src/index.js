b3aa929ce16cc3585d24335b9be7de19e3906447
IrynaMel <iryna.melnyk270692@gmail.com>
2022-07-20 15:06:22 +0300

    fix

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="irynamel.github.io">
+      <BrowserRouter basename="">
         <PersistGate loading={'loading'} persistor={persistor}>
           <App />
         </PersistGate>
